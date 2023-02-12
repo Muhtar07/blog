80ffc349a534f75622136b87453bd8ec55dbe6f7
Muhtar <bmuhtar07@gmail.com>
2023-02-12 23:09:22 +0300

    add jest, jest config

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,37 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first params', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional params', () => {
+        const classResult = 'someClass hello clear';
+        expect(classNames('someClass', {}, ['hello', 'clear']))
+            .toBe(classResult);
+    });
+    test('with mods all true', () => {
+        const classResult = 'someClass hello clear hovered active';
+        expect(classNames('someClass', {
+            hovered: true, active: true,
+        }, ['hello', 'clear']))
+            .toBe(classResult);
+    });
+    test('with mods undefined', () => {
+        const classResult = 'someClass hello clear hovered';
+        expect(classNames('someClass', {
+            hovered: true, active: undefined,
+        }, [
+            'hello', 'clear',
+        ]))
+            .toBe(classResult);
+    });
+    test('with mods false', () => {
+        const classResult = 'someClass hello clear hovered';
+        expect(classNames('someClass', {
+            hovered: true, active: false,
+        }, [
+            'hello', 'clear',
+        ]))
+            .toBe(classResult);
+    });
+});
