78a7bcf29a6412456ddb10d20751cc17617d14e7
Muhtar <bmuhtar07@gmail.com>
2023-02-18 13:39:18 +0300

    fix yml

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,16 +1,15 @@
-import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "../lib/ThemeContext";
-import {FC, useMemo, useState} from "react";
+import { FC, useMemo, useState } from 'react';
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
 
-const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-export  const ThemeProvider: FC = ({children}) => {
-
+export const ThemeProvider: FC = ({ children }) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme);
 
     const defaultProps = useMemo(() => ({
-        theme: theme,
-        setTheme: setTheme,
-    }), [theme])
+        theme,
+        setTheme,
+    }), [theme]);
     return (
         <div>
             <ThemeContext.Provider value={defaultProps}>
@@ -19,4 +18,3 @@ export  const ThemeProvider: FC = ({children}) => {
         </div>
     );
 };
-
